039c71d4a22441be87afbb99aeeeac0899d2ccde
mrvalraven <akatiago@gmail.com>
2021-10-03 18:34:20 +0100

    added form reactivity

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,60 +1,61 @@
 <template>
-  <button  class="button" >{{buttonText}}
-      <a v-if="link" :href="link" rel="noreferrer nooppener" target="_blank"></a>
+  <button class="button">
+    {{ buttonText }}
+    <a v-if="link" :href="link" rel="noreferrer nooppener" target="_blank"></a>
   </button>
 </template>
 
 <script>
 export default {
-    name: 'Button',
-    props: {
-        buttonText: String,
-        link: String,
-    }
-}
+  name: "Button",
+  props: {
+    buttonText: String,
+    link: String,
+  },
+};
 </script>
 
 <style scoped lang="scss">
-$whiteBlue: #E7ECEF;
-$darkBlue: #274C77;
+$whiteBlue: #e7ecef;
+$darkBlue: #274c77;
 $normalBlue: #6aa7cf;
-$lightBlue: #A3CEF1;
+$lightBlue: #a3cef1;
 $easing: cubic-bezier(0.39, 1.61, 0.89, 1.22);
 $fastEasing: cubic-bezier(0.075, 0.82, 0.165, 1);
 
 button {
-    padding: 15px 20px;
-    border: 0;
-    outline: 0;
-    border-radius: 25px;
-    box-shadow: rgba(83, 83, 83, 0.35) 0px 5px 10px;
-    text-transform: uppercase;
-    font-family: 'Metropolis', sans-serif;
-    font-weight: bold;
-    cursor: pointer;
-    font-size: 16px;
-    transition: all 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
+  padding: 15px 20px;
+  border: 0;
+  outline: 0;
+  border-radius: 25px;
+  box-shadow: rgba(83, 83, 83, 0.35) 0px 5px 10px;
+  text-transform: uppercase;
+  font-family: "Metropolis", sans-serif;
+  font-weight: bold;
+  cursor: pointer;
+  font-size: 16px;
+  transition: all 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
 
-    &:nth-child(2) {
-      margin-left: 30px;
-    }
-
-    &:hover {
-      transform: translateY(-5px);
-    }
-
-    a {
-      opacity: 1;
-    }
+  &:nth-child(2) {
+    margin-left: 30px;
   }
+
+  &:hover {
+    transform: translateY(-5px);
+  }
+
+  a {
+    opacity: 1;
+  }
+}
 @media (max-width: 450px) {
-    .button {
-        padding: 15px;
-    }
+  .button {
+    padding: 15px;
+  }
 }
 @media (max-width: 350px) {
-    .button {
-        padding: 8px;
-    }
+  .button {
+    padding: 8px;
+  }
 }
 </style>
--- src/views/Contactos.vue
+++ src/views/Contactos.vue
@@ -7,7 +7,7 @@
   <Navbar v-if="!mobileMode" class="navbar" />
   <div v-if="!activatedNavbar" class="pageContent">
     <section class="contacts">
-      <div class="contactForm">
+      <div class="contactForm" v-if="!sendingEmail">
         <div class="contactInfo">
           <h1>
             Tens alguma questão?<br />
@@ -69,7 +69,7 @@
             v-model="mensagem"
             required
           />
-          <div class="buttons">
+          <div class="buttons" :class="{ isActive: isFormFilled }">
             <Button
               buttonText="Enviar mensagem"
               class="g-recaptcha"
@@ -80,13 +80,34 @@
           </div>
         </form>
       </div>
+      <div class="modal" v-else>
+        <img
+          v-if="!emailSent && !emailFailed"
+          src="@/assets/api/spinning-circle.gif"
+          alt=""
+        />
+        <div v-else-if="emailSent" class="sucess">
+          <h1>Mensagem enviada</h1>
+          <i class="far fa-check-circle sucessIcon"></i>
+          <Button buttonText="Voltar" @click="resetEmailSendingValues" />
+        </div>
+        <div v-else-if="emailFailed" class="failure">
+          <h1>Ocorreu um erro</h1>
+          <i class="fas fa-exclamation-circle errorIcon"></i>
+          <p>
+            Por favor volte a tentar mais tarde ou envie uma mensagem para
+            geral@aaue.pt
+          </p>
+          <Button buttonText="Voltar" @click="resetEmailSendingValues" />
+        </div>
+      </div>
     </section>
     <Footer />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, watch } from "vue";
 import Navbar from "../components/Navbar.vue";
 import NavbarMobile from "../components/NavbarMobile.vue";
 import ScrollToTopButton from "../components/ScrollToTopButton.vue";
@@ -101,10 +122,36 @@ export default defineComponent({
       isAtTop: true,
       mobileMode: false,
       emailSent: false,
+      emailFailed: false,
+      sendingEmail: false,
       name: "",
       email: "",
       assunto: "",
       mensagem: "",
+      isFormFilled: false,
+    };
+  },
+  setup() {
+    let name = ref("");
+    let email = ref("");
+    let assunto = ref("");
+    let mensagem = ref("");
+    let isFormFilled = ref(false);
+
+    watch([name, email, assunto, mensagem], () => {
+      if (name.value && email.value && assunto.value && mensagem.value) {
+        isFormFilled.value = true;
+      } else {
+        isFormFilled.value = false;
+      }
+    });
+
+    return {
+      isFormFilled: isFormFilled,
+      name: name,
+      email: email,
+      assunto: assunto,
+      mensagem: mensagem,
     };
   },
   components: {
@@ -146,6 +193,12 @@ export default defineComponent({
       }
     },
     async sendFormData() {
+      if (!this.isFormFilled) {
+        return;
+      }
+
+      this.sendingEmail = true;
+
       const formData = {
         sendTo: "informativa@aaue.pt",
         subject: this.assunto,
@@ -172,19 +225,25 @@ export default defineComponent({
         .then(async (data) => {
           if (data.ok) {
             this.emailSent = true;
-            this.clearInfo();
+            this.clearFormInfo();
           }
         })
         .catch((error) => {
+          this.emailFailed = true;
           console.log(error.message);
         });
     },
 
-    clearInfo() {
+    clearFormInfo() {
       this.name = "";
       this.email = "";
       this.assunto = "";
       this.mensagem = "";
+    },
+    resetEmailSendingValues() {
+      this.sendingEmail = false;
+      this.emailSent = false;
+      this.emailFailed = false;
     },
   },
 });
@@ -305,6 +364,70 @@ $specialColor: #2c3e50;
 
       .buttons {
         display: flex;
+
+        .button {
+          cursor: not-allowed;
+          opacity: 0.4;
+          border-radius: 0;
+          margin-left: 0;
+
+          &:hover {
+            transform: translateY(0px);
+          }
+        }
+      }
+
+      .isActive {
+        .button {
+          cursor: pointer;
+          opacity: 1;
+
+          &:hover {
+            transform: translateY(-5px);
+          }
+        }
+      }
+    }
+  }
+
+  .modal {
+    width: 500px;
+    min-height: 300px;
+    padding: 20px 50px;
+    border-radius: 25px;
+    background-color: #ffffff;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
+    img {
+      width: 100px;
+      height: 100px;
+    }
+
+    .sucess,
+    .failure {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      .errorIcon {
+        margin: 20px 0 0 0;
+        font-size: 100px;
+        color: #992c2c;
+      }
+
+      .sucessIcon {
+        margin: 35px 0;
+        font-size: 100px;
+        color: #4bb543;
+      }
+
+      p {
+        margin: 20px 0;
       }
     }
   }
